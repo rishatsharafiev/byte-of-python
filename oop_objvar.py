d0e78d7ef4ec2b3ae432cb96b800feed6357560a
Rishat Sharafiev <rishatsharafiev@ya.ru>
2019-02-11 16:33:48 +0300

    Refactor Robot class

--- oop_objvar.py
+++ oop_objvar.py
@@ -11,18 +11,18 @@ class Robot:
 
         # When this person is created, the robot
         # adds to the population
-        Robot.population += 1
+        self.__class__.population += 1
 
     def die(self):
         """I am dying."""
         print("{} is being destroyed!".format(self.name))
 
-        Robot.population -= 1
+        self.__class__.population -= 1
 
-        if Robot.population == 0:
+        if self.__class__.population == 0:
             print("{} was the last one.".format(self.name))
         else:
-            print("There are still {:d} robots working.".format(Robot.population))
+            print("There are still {:d} robots working.".format(self.__class__.population))
 
     def say_hi(self):
         """Greeting by the robot. Yeah, they can do that."""
